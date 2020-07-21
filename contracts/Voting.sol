2bd08663b8297b1aa8c7d3d1684a2a88afc45c90
AA <angelangelov97@gmail.com>
2020-07-22 01:39:48 +0300

    Add minBalance and minTime set at vote creation event

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -73,7 +73,7 @@ contract Voting is IForwarder, AragonApp {
 
     mapping(address => uint256) public lastCreateVoteTimes;
 
-    event StartVote(uint256 indexed voteId, address indexed creator, string metadata);
+    event StartVote(uint256 indexed voteId, address indexed creator, string metadata, uint256 minBalance, uint256 minTime);
     event CastVote(uint256 indexed voteId, address indexed voter, bool supports, uint256 stake);
     event ExecuteVote(uint256 indexed voteId);
     event ChangeSupportRequired(uint64 supportRequiredPct);
@@ -385,7 +385,7 @@ contract Voting is IForwarder, AragonApp {
         vote_.votingPower = votingPower;
         vote_.executionScript = _executionScript;
 
-        emit StartVote(voteId, msg.sender, _metadata);
+        emit StartVote(voteId, msg.sender, _metadata, minBalance, minTime);
 
         lastCreateVoteTimes[msg.sender] = getTimestamp64();
 
