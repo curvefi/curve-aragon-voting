054a9367fb084789e3ac1b36191758b4f563cd62
PJColombo <paulo.colombo@pm.me>
2021-11-18 03:13:07 +0100

    Add aragon-related helper functions for testing

---
+++ test/helpers/events.ts
@@ -0,0 +1,22 @@
+import { Contract } from "@ethersproject/contracts";
+
+export const getEventArgument = async (contract: Contract, txHash: string, eventName: string, eventArg: string): Promise<any> => {
+  const filterFn = contract.filters[eventName]
+
+  if (!filterFn) {
+    throw new Error(`Event ${eventName} not found in contract`)
+  }
+
+  const filter = filterFn()
+  const events = await contract.queryFilter(filter)
+  // Filter both by tx hash and event signature hash
+  const [event] = events.filter(event => event.transactionHash === txHash && event.topics[0] === filter.topics[0])
+
+  const argValue = event.args[eventArg]
+
+  if (!argValue) {
+    throw new Error(`Argument ${eventArg} not found in event ${eventName}`)
+  }
+
+  return argValue
+}
---
+++ test/helpers/index.ts
@@ -0,0 +1,7 @@
+export * from './addresses'
+export * from './acl'
+export * from './apps'
+export * from './dao'
+export * from './events'
+export * from './numbers'
+export * from './time'
