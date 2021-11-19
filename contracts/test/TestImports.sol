0ec1c3dae127e2d6cd29bd20932c4e383fd101c4
PJColombo <paulo.colombo@pm.me>
2021-11-19 18:55:22 +0100

    Remove Truffle mentions from comments

--- contracts/test/TestImports.sol
+++ contracts/test/TestImports.sol
@@ -6,14 +6,13 @@ import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 
 // You might think this file is a bit odd, but let me explain.
-// We only use some contracts in our tests, which means Truffle
+// We only use some contracts in our tests, which means Hardhat
 // will not compile it for us, because it is from an external
 // dependency.
 //
 // We are now left with three options:
 // - Copy/paste these contracts
-// - Run the tests with `truffle compile --all` on
-// - Or trick Truffle by claiming we use it in a Solidity test
+// - Or trick Hardhat by claiming we use it in a Solidity test
 //
 // You know which one I went for.
 
